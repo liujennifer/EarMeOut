5b06e72c9970b037b7020383fb8ea95c6c3591e3
Oscar <oscar.baracos@gmail.com>
2018-02-04 11:38:00 -0500

    Implemented PlayChords

--- lambda_function.py
+++ lambda_function.py
@@ -87,7 +87,7 @@ def on_session_started(session_started_request, session):
 
 
 # ---------- Custom intents-----------------------------------------------------
-def check_answer(answer, interval_number: int):
+def check_interval(answer, interval_number: int):
     correct_interval = interval_names[interval_number]
     if correct_interval == str(answer):
         output = "Correct!"
@@ -100,7 +100,54 @@ def check_answer(answer, interval_number: int):
                           output, None, False))
 
 
-def play_sound():
+def check_chord(answer: str, chord: int):
+    correct_chord = chord_names[chord][0]
+    if correct_chord == str(answer):
+        output = "Correct!"
+    else:
+        output = f'Incorrect! This is a {correct_chord} chord. You said' \
+                 f'{answer}.'
+
+    title = f'Answer: {correct_chord}, response: {answer}'
+
+    return build_response({}, build_speechlet_response(title,
+                          output, None, False))
+
+
+def _note_to_url(note: int):
+    return 'https://raw.githubusercontent.com/liujennifer/EarTrainer/master/' \
+           'notes-mp3/' + str(note) + '.mp3'
+
+
+def play_chord():
+    base_note = random.randint(15, 49)
+    chord = random.randint(0, len(chord_names) - 1)
+    chord_notes = chord_names[chord][1]
+    session_attributes = {"chord": chord}
+
+    urls = [_note_to_url(base_note)]
+    for interval in chord_notes:
+        urls.append(_note_to_url(base_note + interval))
+
+    audio_urls = "".join(["<audio src='" + url + "'/>" for url in urls])
+    response = {
+        'outputSpeech': {
+            'type': 'SSML',
+            'ssml': "<speak>What is this chord?" + audio_urls + "</speak>"
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'SSML',
+                'text': "<speak>What is this chord?" + audio_urls +
+                        "</speak>"
+            }
+        },
+        'shouldEndSession': False
+    }
+    return build_response(session_attributes, response)
+
+
+def play_interval():
     base_note = random.randint(15, 49)
     interval_num = random.randint(0, len(interval_names) - 1)
     session_attributes = {"interval_num": interval_num}
@@ -140,12 +187,18 @@ def on_intent(intent_request, session):
     intent_name = intent['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "PlaySound":
-        return play_sound()
+    if intent_name == "PlayInterval":
+        return play_interval()
+    elif intent_name == "PlayChord":
+        return play_chord()
     elif intent_name == "CheckAnswer":
-        answer = intent['slots']['IntervalName']['value']
-        interval_number = session['attributes']['interval_num']
-        return check_answer(answer, interval_number)
+        answer = intent['slots']['Answer']['value']
+        if session['attributes'].get('chord') is not None:
+            chord = session['attributes']['chord']
+            return check_chord(answer, chord)
+        elif session['attributes'].get('interval_num') is not None:
+            interval_number = session['attributes']['interval_num']
+            return check_interval(answer, interval_number)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == \
@@ -176,3 +229,4 @@ interval_names = {0: "unison", 1: "minor 2nd", 2: "major 2nd",
                   6: "tritone", 7: "perfect 5th", 8: "minor 6th",
                   9: "major 6th", 10: "minor 7th", 11: "major 7th",
                   12: "octave"}
+chord_names = [["major", [4, 7]], ["minor", [3, 7]]]
