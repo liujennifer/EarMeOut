f57d74bdaca0f1a65b688069151d59ee322487e2
Oscar <oscar.baracos@gmail.com>
2018-02-04 10:55:23 -0500

    Update interval_name dictionary Include complete set of interval names

--- lambda_function.py
+++ lambda_function.py
@@ -166,10 +166,10 @@ def lambda_handler(event, context):
         return on_session_ended(event['request'], event['session'])
 
 
-interval_names = {0: "major 6th", 1: "minor 3rd", 2: "perfect 4th",
-                  3: "unison", 4: "tritone"}
-# interval_names = {0: "unison", 1: "minor 2nd", 2: "major 2nd",
-#                   3: "minor 3rd", 4: "major 3rd", 5: "perfect 4th",
-#                   6: "perfect 5th", 7: "tritone", 8: "minor 6th",
-#                   9: "major 6th", 10: "minor 7th", 11: "major 7th",
-#                   12: "octave"}
+# interval_names = {0: "major 6th", 1: "minor 3rd", 2: "perfect 4th",
+#                   3: "unison", 4: "tritone"}
+interval_names = {0: "unison", 1: "minor 2nd", 2: "major 2nd",
+                  3: "minor 3rd", 4: "major 3rd", 5: "perfect 4th",
+                  6: "tritone", 7: "perfect 5th", 8: "minor 6th",
+                  9: "major 6th", 10: "minor 7th", 11: "major 7th",
+                  12: "octave"}
